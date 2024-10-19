f6fb8c092b9ac2f7b5b77dc6242f4548d94669af
Alebproject32 <isaacale25@gmail.com>
2024-10-19 11:18:44 -0400

    My Programming to encapsutaling with Scripture Memorizer Program

--- Prep3/obj/Debug/net8.0/Prep3.AssemblyInfo.cs
+++ Prep3/obj/Debug/net8.0/Prep3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prep3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba613c15f72314f7c7ca737b959390d0e39dc65d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c127802d32da0ecfb58a17a4889a83ded296db1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prep3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prep3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
