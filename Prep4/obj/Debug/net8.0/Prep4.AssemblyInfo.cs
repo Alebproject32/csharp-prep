7645ca7b8ebd1cb519f1b6092f7460295d0322f7
Alebproject32 <isaacale25@gmail.com>
2024-10-23 20:48:41 -0400

    Program

--- Prep4/obj/Debug/net8.0/Prep4.AssemblyInfo.cs
+++ Prep4/obj/Debug/net8.0/Prep4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prep4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c127802d32da0ecfb58a17a4889a83ded296db1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6fb8c092b9ac2f7b5b77dc6242f4548d94669af")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prep4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prep4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
